2b11a8ee4ebf94640f517d9d04e9e48849dcf894
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-09-11 17:34:16 +0900

    update

--- scripts/zenn-cli.sh
+++ scripts/zenn-cli.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-mkdir -p ~/.npm-global
-npm config set prefix '~/.npm-global'
+# mkdir -p ~/.npm-global
+# npm config set prefix '~/.npm-global'
 
-export PATH=~/.npm-global/bin:$PATH
-source ~/.profile
+# export PATH=~/.npm-global/bin:$PATH
+# source ~/.profile
 
 npm init --yes
-npm install -g zenn-cli
+npm install -g zenn-cli@latest
 
-sudo npx zenn init
+npx zenn init
 
+# 🎉 Done!
+# 早速コンテンツを作成しましょう
+
+# 👇 新しい記事を作成する
+# $ zenn new:article
+
+# 👇 新しい本を作成する
+# $ zenn new:book
+
+# 👇 投稿をプレビューする
+# $ zenn preview
+
+npx zenn new:article
 npx zenn preview
